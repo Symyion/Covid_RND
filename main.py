377ffeb11113941a48bcf945cdc326e1e1b4d780
symyion <Symyion@gmail.com>
2021-02-15 19:06:25 +0100

    Started adding methods for initial data prep of option chain data. Converted method intended for 1 index to method for multiple methods.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import tools
 connection = sqlite3.connect("data/option_data.db")
 print(connection.total_changes)
 
-AEX = tools.ChainID('data/index_keys.csv', 'AEX')
-AEX.transform_long('data/aex.xlsx')
+chains = tools.OptionChain('data/index_keys.csv')
+chains.convert_chains('data/chains.xlsx')
 #cursor = connection.cursor()
 #cursor.execute("CREATE TABLE chains (obs_month TEXT, index_name TEXT, maturity INTEGER, chain_id TEXT)")
 
--- tools.py
+++ tools.py
@@ -1,22 +1,14 @@
 import pandas as pd
 
 
-class ChainID:
-    excel_path = "data/chains.xlsx"
-
-    def __init__(self, data_path):
-        self.path = data_path
-
-    def transform_long(self, excel_path):
-        wide = pd.read_csv(self.path)
-        long = wide.stack().reset_index(level=1).reset_index(level=0, drop=True).dropna()
-        long.columns = ['obs_month', 'chain_key']
-        return long.drop_duplicates(subset=['chain_key']).to_excel(excel_path, index=False)
-
-    def convert_chains(self, excel_path):
+class OptionChain:
+    def __init__(self, csv_data_path='data/data.csv'):
+        self.path = csv_data_path
         raw_data = pd.read_csv(self.path, header=[0, 1])
         data_transposed = pd.DataFrame(raw_data.T.stack())
         data_transposed = data_transposed.reset_index(level=2, drop=True).reset_index(level=1).reset_index(level=0)
         data_transposed.columns = ['obs_month', 'index', 'chain_id']
-        data_transposed = data_transposed.drop_duplicates(subset=['chain_id'])
-        return data_transposed.to_excel(excel_path, index=False)
+        self.data_transposed = data_transposed.drop_duplicates(subset=['chain_id'])
+
+    def convert_chains(self, excel_path="data/chains.xlsx"):
+        return self.data_transposed.to_excel(excel_path, index=False)
