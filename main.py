7b025eb82532b972d4031fe46bac8aa6e9c05826
symyion <Symyion@gmail.com>
2021-02-16 20:13:58 +0100

    Added functionality to split option call and put data.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import tools
 connection = sqlite3.connect("data/option_data.db")
 print(connection.total_changes)
 
-chains = tools.OptionChain('data/index_keys.csv')
-chains.convert_chains('data/chains.xlsx')
+chains = tools.ChainConverter('data/index_keys.csv')
+chains.write_calls_to_excel()
 #cursor = connection.cursor()
 #cursor.execute("CREATE TABLE chains (obs_month TEXT, index_name TEXT, maturity INTEGER, chain_id TEXT)")
 
--- tools.py
+++ tools.py
@@ -1,14 +1,41 @@
 import pandas as pd
 
 
-class OptionChain:
+def transpose_data(raw_data):
+    data_t = pd.DataFrame(raw_data.T.stack())
+    data_t = data_t.reset_index(level=2, drop=True).reset_index(level=1).reset_index(level=0)
+    data_t.columns = ['obs_month', 'index', 'chain_id']
+    data_t = data_t.drop_duplicates(subset=['chain_id'])
+    return data_t
+
+
+class OptionChainConverter:
     def __init__(self, csv_data_path='data/data.csv'):
         self.path = csv_data_path
-        raw_data = pd.read_csv(self.path, header=[0, 1])
-        data_transposed = pd.DataFrame(raw_data.T.stack())
-        data_transposed = data_transposed.reset_index(level=2, drop=True).reset_index(level=1).reset_index(level=0)
-        data_transposed.columns = ['obs_month', 'index', 'chain_id']
-        self.data_transposed = data_transposed.drop_duplicates(subset=['chain_id'])
+        self.data = transpose_data(pd.read_csv(self.path, header=[0, 1], dtype='str'))
 
-    def convert_chains(self, excel_path="data/chains.xlsx"):
-        return self.data_transposed.to_excel(excel_path, index=False)
+    def get_all(self, data_path="data/chains.xlsx", write_excel=False):
+        if write_excel:
+            self.data.to_excel(data_path, index=False)
+        return self.data
+
+    def get_calls(self, call_path='data/calls.xlsx', write_excel=False):
+        calls = self.data[self.data.chain_id.str.contains('#C', regex=True, na=False)]
+
+        if write_excel:
+            calls.to_excel(call_path, index=False)
+        return calls
+
+    def get_puts(self, put_path='data/put.xlsx', write_excel=False):
+        puts = self.data[self.data.chain_id.str.contains('#P', regex=True, na=False)]
+
+        if write_excel:
+            puts.to_excel(put_path, index=False)
+        return puts
+
+
+class OptionDescriptionExtractor:
+    def __init__(self, csv_description_path):
+        self.path = csv_description_path
+        self.raw_data = pd.read_csv(self.path)
+        # TODO split description column into strike price, strike date & index name
